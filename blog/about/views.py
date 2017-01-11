afac9a8ee02dcad1e9411bfddc1931234c30c82e
Harsh Rastogi <harsh@droidharsh.com>
2017-01-10 23:28:44 -0800

    Changed resume file format to pdf from docx

--- blog/about/views.py
+++ blog/about/views.py
@@ -11,9 +11,9 @@ def index(request):
 
 
 def download_resume(request):
-    filename = 'D:/mywebsite/blog/about/downloadable/resumeharsh.docx'
+    filename = 'D:/mywebsite/blog/about/downloadable/resume.pdf'
     data = open(filename, "rb").read()
     response = HttpResponse(data, content_type='application/vnd')
-    response['Content-Disposition'] = 'attachment; filename=resume_harsh.docx'
+    response['Content-Disposition'] = 'attachment; filename=resume_harsh.pdf'
     response['Content-Length'] = os.path.getsize(filename)
     return response
