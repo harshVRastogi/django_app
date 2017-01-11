f24d74b66b943d9f6e4e55010542742edba80a4d
Harsh Rastogi <harsh@droidharsh.com>
2017-01-10 23:16:30 -0800

    Added url pattern  for resume download

--- blog/about/urls.py
+++ blog/about/urls.py
@@ -1,6 +1,8 @@
 from django.conf.urls import url
 from . import views
 
+app_name = "about"
 urlpatterns = [
-    url(r'^$', views.index, name='index')
+    url(r'^$', views.index, name='index'),
+    url(r'^download/$', views.download_resume, name='download')
 ]
